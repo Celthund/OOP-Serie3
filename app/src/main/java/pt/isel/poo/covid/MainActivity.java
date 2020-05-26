7646f8bc445b8aa0b8292635bdfa4b864b3269a2
Tiago Silva <tiagosik1994@gmail.com>
2020-05-26 23:32:06 +0100

    Change from AppCompatActivity to Activity.

--- app/src/main/java/pt/isel/poo/covid/MainActivity.java
+++ app/src/main/java/pt/isel/poo/covid/MainActivity.java
@@ -1,10 +1,9 @@
 package pt.isel.poo.covid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
